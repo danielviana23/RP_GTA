befc0882bdd7a54aaf0af06d1bc66a67b7d81b96
danielviana23 <danielviana.dev@gmail.com>
2022-10-29 18:29:10 -0300

    adicionando filtro categoria

--- src/pages/mochila/MochilaPage.js
+++ src/pages/mochila/MochilaPage.js
@@ -6,7 +6,12 @@ import ServiceJogador from '../../services/JogadorService';
 function MochilaPage() {
 
     const [itensMochilaJogador, setItensMochilaJogador] = useState([]);
-
+    const filtro_categoria = {
+      comida: "comida",
+      bebida: "bebida",
+      arma: "arma",
+      outros_produtos: "outros_produtos"
+    };
     const itens_mochila_jogador = ServiceJogador.buscar_itens_mochila_jogador(); 
     
     function mostrarDropdown(id_item) {
@@ -19,17 +24,26 @@ function MochilaPage() {
     }
 
     function mostrarComidas() {
-      return itens_mochila_jogador.map(item => {
-        return (
-          <li className='item_mochila' id='lista_categoria_comida'>
-            <div className='nome_produto_mochila' >Pizza ( 10 unidades )</div>
-            <div className='botoes_usar_transferir'>
-              <div className='botao_usar_mochilapage'>Usar</div>
-              <div className='botao_transferir_mochilapage'>Transferir</div>
-            </div>
-          </li>
-        )
+       let itens_comida_filtrados = itens_mochila_jogador.map(item => {
+        if(item.categoria === filtro_categoria.comida) {
+          return (
+            <li className='item_mochila' id='lista_categoria_comida'>
+              <div className='nome_produto_mochila' >Pizza ( 10 unidades )</div>
+              <div className='botoes_usar_transferir'>
+                <div className='botao_usar_mochilapage'>Usar</div>
+                <div className='botao_transferir_mochilapage'>Transferir</div>
+              </div>
+            </li>
+          )
+        }
       });
+
+      if(itens_comida_filtrados.length == 0) {
+        alert("foi")
+        return <h1>Nenhum produto encontrado</h1>
+      }
+
+      return itens_comida_filtrados;
     }
 
     function mostrarBebidas() {
--- src/services/JogadorService.js
+++ src/services/JogadorService.js
@@ -5,12 +5,30 @@ const ServiceJogador = {
 
     buscar_itens_mochila_jogador: function buscarItensMochilaJogador() {
         return [
+            // {
+            //     id_item: 1,
+            //     nome_produto: "Pizza",
+            //     categoria: "comida",
+            //     quantidade: 10
+            // }, 
             {
-                id_item: 1
+                id_item: 2,
+                nome_produto: "Pistola",
+                categoria: "arma",
+                quantidade: 1
             }, 
             {
-                id_item: 2
-            }
+                id_item: 3,
+                nome_produto: "Pistola",
+                categoria: "bebida",
+                quantidade: 1
+            }, 
+            {
+                id_item: 4,
+                nome_produto: "Relógio",
+                categoria: "outros_produtos",
+                quantidade: 1
+            } 
         ];
     },
 }
