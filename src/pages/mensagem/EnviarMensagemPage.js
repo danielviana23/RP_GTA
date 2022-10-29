38f07348a82409a75b1adc2742696e53aa71e8b5
danielviana23 <danielviana.dev@gmail.com>
2022-10-28 21:56:47 -0300

    adicionando mais telas

--- src/pages/mensagem/EnviarMensagemPage.js
+++ src/pages/mensagem/EnviarMensagemPage.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import HeaderComponentVoltar from '../../components/footer/headerVoltar/HeaderComponentVoltar';
-import './MensagemPage.css';
+import './EnviarMensagemPage.css';
 
 function EnviarMensagemPage() {
 
     function enviarMensagemPage() {
-        
+        alert("mensagem enviada com sucesso")
     }
 
     return (
-      <div id="enviar_mensagem_page">
-        <div id='titulo_escrever_mensagem'>
-            <h1>Enviar mensagem</h1>
-        </div>
-        <div id='numero_destinatario'>
-            <div>
-                Destinatario
+      <div id="body_pagina_enviar_mensagem">
+        <div id='enviar_mensagem_page'>
+            <div id='titulo_escrever_mensagem'>
+                <h1 id='titulo_enviar_mensagem'>Enviar mensagem</h1>
             </div>
-            <input />
+            <div id='numero_destinatario'>
+                <div id='descricao_destinatario_mensagem'>
+                    N° Destinatário
+                </div>
+                <input id='input_numero_destinatario'/>
+            </div>
+            <textarea id='text_area_conteudo_mensagem' placeholder='Escreva sua mensagem'>
+            </textarea>
+            <button id='botao_enviar_mensagem' onClick={enviarMensagemPage}>
+                Enviar mensagem
+            </button>
+            <HeaderComponentVoltar href="/mensagem"/>
         </div>
-        <textarea id='text_area_conteudo_mensagem' placeholder='Escreva sua mensagem'>
-        </textarea>
-        <HeaderComponentVoltar href="/mensagem"/>
       </div>
     );
 }
--- src/pages/mochila/MochilaPage.js
+++ src/pages/mochila/MochilaPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderComponentVoltar from '../../components/footer/headerVoltar/HeaderComponentVoltar';
+import './MochilaPage.css';
 
 function MochilaPage() {
     return (
       <div className="mensagem_page">
+        <div>
+          <h1>Sua mochila</h1>
+        </div>
+        <span id='capacidade_mochila'>Capacidade de carga: 10 unidades</span>
+        <ul id='lista_itens_mochila'>
+          <li className='item_mochila' id='item_comida_mochila'><Link className='link_page_mochila' to={"/jogador/mochila/comida"}>Comida</Link></li>
+          <li className='item_mochila' id='item_arma_mochila'><Link className='link_page_mochila' to={"/jogador/mochila/armas"}>Armas</Link></li>
+          <li className='item_mochila' id='item_outros_produtos_mochila'><Link className='link_page_mochila' to={"/jogador/mochila/outros_produtos"}>Outros produtos</Link></li>
+        </ul>
         <HeaderComponentVoltar href="/"/>
       </div>
     );
